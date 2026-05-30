4b8230b092c4383befb003439fb9a8a6e5490a30
masfeb15 <febrisugianto85@gmail.com>
2026-05-30 23:17:35 +0700

    Update supabaseClient.js

--- public/js/supabaseClient.js
+++ public/js/supabaseClient.js
@@ -1,1 +1,5 @@
+const SUPABASE_URL = "https://YOUR_PROJECT.supabase.co";
+const SUPABASE_ANON_KEY = "YOUR_ANON_KEY";
 
+const { createClient } = supabase;
+const sb = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
