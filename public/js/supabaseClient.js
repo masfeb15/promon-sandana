c49f1f8705ffe7e231ef57a4b2f153f380142710
masfeb15 <febrisugianto85@gmail.com>
2026-06-14 19:44:32 +0700

    Delete public directory

--- public/js/supabaseClient.js
+++
@@ -1,5 +0,0 @@
-const SUPABASE_URL = "https://pkocozkqixozyfyxornf.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_oF3PveJANVV2cIphJntjNg_FrL2Mv8G";
-
-const { createClient } = supabase;
-const sb = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
